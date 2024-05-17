73cf67bc639cf6b44ed72672800e99c81fdf7c40
darelljay <darelljason1@gmail.com>
2024-05-18 08:50:43 +0900

    update: "주석제거"

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import express from "express";
-// import bodyParser from "body-parser";
-// import * as business from "./business";
+
 import {initAppExpress} from './server/initAppExpress'
 const app = express();
 const port = process.env.PORT || 3000; 
@@ -11,50 +10,6 @@ console.log({
   password: process.env.DB_DATABASE
 });
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:false}));
-
-// 2.009s
-
-// app.get("/users/:id/vocabulary/level", async (req, res) => {
-//   // console.log(parseInt(req.params.id));
-  
-//   const userId = business.getNumber(parseInt(req.params.id));
-//   const result = await business.getUserVocabularyLevel(userId);
-//   res.json(result);
-
-//   const start = Date.now()
-	
-// 	setTimeout(() => {
-// 	  const millis = Date.now() - start;
-
-// 	  console.log(`seconds elapsed = ${millis / 1000}`);
-// 	  // expected output: seconds elapsed = 2
-// 	}, 2000);
-// });
-
-// 2.015
-
-// app.post("/users/word", async (req, res) => {
-//   console.log(req.body)
-//   const userId = business.getNumber(req.body.id);
-//   const word = req.body.word;
-//   try {
-//     await business.postUserWord(userId, word);
-//     res.json(true);
-//   } catch (error) {
-//     res.json(false);
-//   }
-
-//   const start = Date.now()
-	
-// 	setTimeout(() => {
-// 	  const millis = Date.now() - start;
-
-// 	  console.log(`seconds elapsed = ${millis / 1000}`);
-// 	  // expected output: seconds elapsed = 2
-// 	}, 2000);
-// });
 
 app.listen(port, () => {
   initAppExpress(app);
